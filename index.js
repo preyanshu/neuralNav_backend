4266b4ddff63abadf52ec6025c4a5eb4b7e56e3f
preyanshu <118627824+preyanshu@users.noreply.github.com>
2023-08-02 22:18:57 +0530

    Add files via upload
    added open ai nodejs backend

--- index.js
+++ index.js
@@ -1,38 +1,53 @@
 
 const express=require("express")
 var cors = require('cors')
+require("dotenv").config()
 
-const spawner=require("child_process").spawn;
+// const spawner=require("child_process").spawn;
 const app=express()
 const port=5000; 
+const { Configuration, OpenAIApi } = require("openai");
 
 
 app.use(cors())
 app.use(express.json())
 
-app.post('/api', function (req, res) {
-    console.log(req.body);
+app.post('/api', async function (req, res) {
+    try {
+        console.log(req.body);
 
     const data_to_pass_in = req.body.message;
     console.log(data_to_pass_in);
+    const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+      });
+    const openai = new OpenAIApi(configuration);
+
+    
+        const completion = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [{"role": "assistant", "content": "You are a helpful assistant."}, {role: "user", content:data_to_pass_in }],
+          });
+          console.log("working");
+          res.json({value:completion.data.choices[0].message.content})
+
+        
+    } catch (error) {
+        // console.log(completion.data.choices[0].message)
+        console.error(error
+            );
+        res.json({value:error.message+":  token expired"})
+        
+    }
+    
+
+   
+
+      
+      
   
 
-    const python_process=spawner("python",["python.py",(data_to_pass_in)]);
-python_process.stdout.on("data",(data) =>{
-    
-    console.log(data.toString());
    
-    var obj= {
-        value : data.toString()
-
-    }
-
-    res.json({hi:"hi"});
-
-
-    
-
-});
     
 
 
